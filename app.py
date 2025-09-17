02cd29904b31510c1d8333564bb00acaf37cc020
chimken0nuggie <hana.suzuki@sjsu.edu>
2025-09-16 17:31:16 -0700

    commit

--- app.py
+++ app.py
@@ -220,5 +220,11 @@ def index():
     return render_template('index.html', results=results, error=error, mdd_list=mdd_list, search_type=search_type, 
                            query=query_value,rows=rows)
 
+@app.route('/nav', methods=['GET'])
+def nav():
+  if request.method == 'GET':
+      return render_template('nav.html')
+  return render_template('nav.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
