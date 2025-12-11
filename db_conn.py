e28aa82284230c14024ff4954094c5d6c90bae14
chimken0nuggie <hana.suzuki@sjsu.edu>
2025-12-10 19:27:58 -0800

    database page added and stores inquiries from users🥳

--- db_conn.py
+++ db_conn.py
@@ -1,6 +1,7 @@
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
+import MySQLdb
 
 load_dotenv()
 
@@ -12,3 +13,33 @@ def init_app(app):
     app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
     app.config['MYSQL_DB'] = os.getenv('DB_NAME')
     
+    mysql.init_app(app)
+
+def create_database():
+    host = os.getenv('DB_HOST')
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    db_name = os.getenv('DB_NAME')
+    port = int(os.getenv('DB_PORT', 3306))
+
+    print(f"Connecting to MySQL server at {host}:{port} as {user}...")
+
+    conn = MySQLdb.connect(host=host, user=user, passwd=password, port=port)
+    cur = conn.cursor()
+    cur.execute("CREATE DATABASE IF NOT EXISTS DGIT")
+    cur.execute(f"USE DGIT")
+    cur.execute(f"""create table IF NOT EXISTS messages (
+                id int auto_increment primary key,
+                name varchar(100),
+                email varchar(100),
+                message text not null,
+                time timestamp default current_timestamp)""")
+    cur.execute(f"""CREATE TABLE IF NOT EXISTS search_history (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                drug_name VARCHAR(255) NOT NULL,
+                gene_name VARCHAR(255) NOT NULL,
+                interaction_type VARCHAR(255),
+                confidence_score FLOAT)""")
+    conn.commit()
+    cur.close()
+    conn.close()
